f9efd8fc17279d0442d6b6bc13efd701630789b1
Andrea <andreachiusole00@gmail.com>
2021-07-17 00:35:31 +0200

    Improved function comments.
    Added some checks in order to skip non morseable chars and duplicates of whitespaces.
    
    Added update of flags

--- src/print_morse.c
+++ src/print_morse.c
@@ -4,70 +4,105 @@
 
 #include "conversion.h"
 
-void append_to_morse_file(FILE *dest, const char input, char *temp)
+/*------------------------------------------------------- *
+* Print at dest the result of the translation of input    *
+* into morse code. The parameter char_sep is used to add  *
+* the separator between letters before translating.       *
+* ------------------------------------------------------- */
+void append_to_morse_file(FILE *dest, const char input, int char_sep)
 {
     char ch_morse[7] = "\0";
+
+    // Concatenate the separator between chars
+    if (char_sep)
+    {
+        strcat(ch_morse, CHARSEP);
+    }
     to_morse(ch_morse, input);
+
     fprintf(dest, "%s", ch_morse);
-    //printf("morse: '%c' = '%s'\n", input, ch_morse);
-    strcat(temp, ch_morse);
 }
 
 
+/*------------------------------------------------------- *
+* Reads the content of source char by char and after      *
+* skipping non morseable chars and duplicates of spaces   *
+* and newlines calls the function to print the result of  *
+* the conversion in destination.                          *
+* The values of the flags initial_whitespace and          *
+* word_finished are updated at the end of every cycle.    *
+* ------------------------------------------------------- */
 void print_morse(FILE *source, FILE *destination)
 {
     char ch_curr = fgetc(source), ch_next;
     int initial_whitespace = TRUE;
-    char temp[1000] = "\0";
+    int word_finished = TRUE;
+
     while (!feof(source))
     {
         ch_next = fgetc(source);
-        // check next char isn't EOF so we can continue
+        // Check if next char is EOF to print last char
         if (feof(source))
         {
-            printf("final charachter: '%c' '%c'\n", ch_curr, ch_next);
-            append_to_morse_file(destination, ch_curr, temp);
+            append_to_morse_file(destination, ch_curr, !word_finished);
             break;
         }
 
-        printf("curr:'%c' next:'%c'\n", ch_curr, ch_next);
+        // Skip non morseable char until a morsable one is reached
+        if (!(is_morseable(ch_next) ||
+              ch_next == SPACE ||
+              ch_next == NEWLINE)
+            )
+        {
+            continue;
+        }
 
-        /* skip whitespace if before the first char */
-        if ((is_space(ch_curr) || is_newline(ch_curr)) &&
+        // Skip whitespaces before the first char
+        if ((ch_curr == SPACE || ch_curr == NEWLINE) &&
              initial_whitespace
             )
         {
-            /* check if ch_next is the first morseable char */
+            // Check if ch_next is the first char
             if (is_morseable(ch_next))
             {
                 initial_whitespace = FALSE;
             }
-            printf("whitespace skipped\n");
-
             ch_curr = ch_next;
             continue;
         }
 
-        // skip duplicates: space+space or space+newline
-        //                  or newline+newline
-        if (is_space(ch_curr)   && is_newline(ch_next) ||
-            is_space(ch_curr)   && is_space(ch_next) ||
-            is_newline(ch_curr) && is_newline(ch_next)
+        // Skip: duplicates (space+space or newline+newline)
+        //       and space before space+newline
+        if ((ch_curr == SPACE)   && (ch_next == SPACE) ||
+            (ch_curr == NEWLINE) && (ch_next == NEWLINE) ||
+            (ch_curr == SPACE)   && (ch_next == NEWLINE)
             )
         {
-            printf("\tskipped one blank char\n");
             ch_curr = ch_next;
             continue;
         }
 
-        if (is_newline(ch_curr) && is_space(ch_next))
+        append_to_morse_file(destination, ch_curr, !word_finished);
+
+        // Update flag for initial whitespace.
+        // The checks done to skip duplicates prevent from having
+        // other scenarios
+        if ((ch_curr == NEWLINE) && (ch_next == SPACE))
         {
             initial_whitespace = TRUE;
         }
 
-        append_to_morse_file(destination, ch_curr, temp);
+        //Update flag for word end reached.
+        if (is_morseable(ch_curr) &&
+            (ch_next == SPACE || ch_next == NEWLINE))
+        {
+            word_finished = TRUE;
+        }
+        else if (is_morseable(ch_curr) && is_morseable(ch_next))
+        {
+            word_finished = FALSE;
+        }
+
         ch_curr = ch_next;
     }
-
-    printf("char temp: '%s'\n", temp);
 }
