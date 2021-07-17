717ae8f3d6eeef856e6f5482bd6a5a114bc73011
Andrea <andreachiusole00@gmail.com>
2021-07-18 00:33:23 +0200

    Added personal file conversion feature

--- src/main.c
+++ src/main.c
@@ -1,51 +1,64 @@
-/*
-*   Programma che legge un file di testo e lo converte in codice MORSE
-*
-*   PREMESSE:
-*
-*   - Le lettere ripetute (doppie) dovrebbero essere riportate una sola volta, ma
-*   per poter fare una eventuale conversione da Morse a testo ho scelto di riportarle
-*   come sono presenti nel source file.
-*
-*   - Non viene fatta distinzione tra lettere maiuscole o minuscole
-*
-*   - I segni di punteggiatura dal source file verranno ignorati e quindi non convertiti.
-*
-*   - Per una leggibilità più chiara la separazione tra lettere viene indicata con '/'
-*   mentre quella tra parole con '//' all'interno del testo convertito.
-*
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>     // # define EXIT_FAILURE 1
 #include <string.h>
 
 #include "conversion.h"
 
 #define INPUT   "./tests/text"
-#define OUTPUT  "./tests/morse"
+#define OUTPUT  "./out/morse.txt"
+
+void start()
+{
+    printf("\n                             TEXT TO MORSE");
+    printf("\nauthor:        Andrea Chiusole");
+    printf("\nbadge number:  209660\n");
+    printf("\n     The scope of this program is to convert text into Morse code.\n");
+    printf("\n If you want to convert a text file of yours you have to place it in the");
+    printf("\n           project folder and afterwards type here its name.\n");
+    printf("\n  The name should be less than 31 charachters including the extension.\n");
+    printf("\n    (A default text file is already present inside the directory.");
+    printf("\n        If you want to convert that one don't type anything)\n");
+    printf("\nWhat is the name of the file? [Max 31 chars]");
+    printf("\n   >>> ");
+}
 
 
 int main(int argc, char *argv[])
 {
+    char source_name[32];
     FILE *source, *destination;
 
-    source = fopen(INPUT, "r");
+    start();
+
+    if (fgets(source_name , 31, stdin) != NULL)
+    {
+        // Strip the newline char if present
+        source_name[strcspn(source_name, "\n")] = '\0';
+    }
+
+    if (!strcmp(source_name, ""))
+    {
+        strcpy(source_name, INPUT);
+    }
+
+    printf("source_name is: %s", source_name);
+
+
+    source = fopen(source_name, "r");
     if (source == NULL)
     {
-        perror("Error opening file");
+        perror("Error opening source file");
         exit(EXIT_FAILURE);
     }
 
     destination = fopen(OUTPUT, "w+");
     if (destination == NULL) {
-        perror("Error opening file");
+        perror("Error opening destination file");
         exit(EXIT_FAILURE);
     }
 
     print_morse(source, destination);
 
-    printf("End of file reached successfully.\n");
     fclose(source);
     fclose(destination);
     return 0;
